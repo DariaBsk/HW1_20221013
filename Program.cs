088f2ece36a1f1ae53dfc818a6992e6a19932e8c
DariaBsk <Dariabsk602@gmail.com>
2022-10-15 20:18:52 +0300

    Добавили решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая
+//1. на вход принимает два числа
+//2. и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Ведите первое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите второе число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b);
+
+{
+   { Console.Write(a = b); }
+
+
+
+{
+    if (a < b); 
+
+
+}}
+
+
+{
+    
+    { Console.Write("max = ", b); }
+
+
+   
+     
+{
+    if (a > b) ;
+    
+    { Console.Write("max = ", a); }
+
+}}
